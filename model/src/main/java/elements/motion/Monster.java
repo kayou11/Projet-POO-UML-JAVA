739bfb34e4d11d7d69213065f75672a7ae60e080
mederic <mederic@10.133.128.239>
2016-06-15 11:31:17 +0200

    class Monster

---
+++ model/src/main/java/elements/motion/Monster.java
@@ -0,0 +1,16 @@
+package elements.motion;
+
+import elements.Sprite;
+
+public class Monster extends MotionElements{
+
+	public Monster(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void getAnimate(){
+	 
+	}
+
+
+}
