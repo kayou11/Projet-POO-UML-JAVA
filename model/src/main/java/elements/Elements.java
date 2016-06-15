5e8df22e985e353c50a291970c2b2f4b9b1e793b
YannQuemar <yann.quemar1@gmail.com>
2016-06-15 09:20:05 +0200

    Class "elements" + corked code in package model

--- model/src/main/java/elements/Elements.java
+++ model/src/main/java/elements/Elements.java
@@ -1,5 +1,35 @@
 package elements;
 
 public abstract class Elements {
+	public void Elements() {
 
+	}
+
+	public Sprite getSprite() {
+
+	}
+
+	public void loadSprite(int idElemet) {
+
+	}
+
+	public void setSprite(Sprite sprite) {
+
+	}
+
+	public Permeability getPermeability() {
+
+	}
+
+	public void setPermeability(Permeability permeability) {
+
+	}
+
+	public ILevel getLevel() {
+
+	}
+
+	public void setLevel(ILevel level) {
+
+	}
 }
