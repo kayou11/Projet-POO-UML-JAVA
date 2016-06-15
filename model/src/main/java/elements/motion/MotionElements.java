f7d405dee131a483685f4ac12c54106d0cc15890
mederic <mederic@10.133.128.239>
2016-06-15 11:08:03 +0200

    CodeBouchoné MotionElements

--- model/src/main/java/elements/motion/MotionElements.java
+++ model/src/main/java/elements/motion/MotionElements.java
@@ -1,5 +1,59 @@
 package elements.motion;
 
-public class MotionElements {
+import java.util.logging.Level;
+import elements.Sprite;
 
+public class MotionElements {
+	
+	private int x;
+	private int y;
+
+	public MotionElements(Sprite sprite){
+		
+	}
+	
+	public int getX(){
+		return 0;
+	}
+	
+	protected void setX(int x){
+		
+	}
+	
+	public int getY(){
+		return 0;
+	}
+	
+	protected void setY(int y){
+		
+	}
+	
+	public void setLevel(Level level, int x, int y){
+		
+	}
+	
+	protected void isMovePossible(int x, int y){
+		
+	}
+	
+
+	private void moveUp(){
+		
+	}
+	
+	private void moveDown(){
+		
+	}
+	
+	private void moveRight(){
+		
+	}
+	
+	private void moveLeft(){
+		
+	}
 }
+
+
+
+
