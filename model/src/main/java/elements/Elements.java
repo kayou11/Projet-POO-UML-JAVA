65bdc1b3bb375b7c55a2fe2077cf65d61acf6941
YannQuemar <yann.quemar1@gmail.com>
2016-06-15 10:33:06 +0200

    Code package elements (classes Position, Elements, Permeability, Sprite)

--- model/src/main/java/elements/Elements.java
+++ model/src/main/java/elements/Elements.java
@@ -1,35 +1,50 @@
 package elements;
 
+import java.util.logging.Level;
+
 public abstract class Elements {
-	public void Elements() {
+	private ISprite sprite;
+	private Permeability permeability;
+	private Level level;
+	private Position position;
 
+	public void Elements(Position position) {
+		setSprite(this.sprite);
+		setPermeability(this.permeability);
 	}
 
-	public Sprite getSprite() {
-
+	public ISprite getSprite() {
+		return this.sprite;
 	}
 
-	public void loadSprite(int idElemet) {
-
+	public void loadSprite(int idElement) {
 	}
 
-	public void setSprite(Sprite sprite) {
-
+	public void setSprite(ISprite sprite) {
+		this.sprite = sprite;
 	}
 
 	public Permeability getPermeability() {
-
+		return this.permeability;
 	}
 
 	public void setPermeability(Permeability permeability) {
-
+		this.permeability = permeability;
 	}
 
-	public ILevel getLevel() {
-
+	public Level getLevel() {
+		return this.level;
 	}
 
-	public void setLevel(ILevel level) {
+	public void setLevel(Level level) {
+		this.level = level;
+	}
 
+	public Position getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
 	}
 }
