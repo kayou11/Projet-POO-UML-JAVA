8deecdebbdca596ddb34765b0444c37da6f77f9c
YannQuemar <yann.quemar1@gmail.com>
2016-06-17 15:09:27 +0200

    Insert "win"

--- model/src/main/java/elements/motion/Lorann.java
+++ model/src/main/java/elements/motion/Lorann.java
@@ -5,61 +5,70 @@ import java.awt.event.KeyEvent;
 import elements.Position;
 import elements.Sprite;
 
-public class Lorann extends MotionElements{
-	
+public class Lorann extends MotionElements {
+
 	static Sprite lorannUp = new Sprite(1);
 	static Sprite lorannDown = new Sprite(2);
-	static Sprite lorannRight= new Sprite(3);
+	static Sprite lorannRight = new Sprite(3);
 	static Sprite lorannLeft = new Sprite(4);
 	static Sprite lorannUpRight = new Sprite(5);
 	static Sprite lorannUpLeft = new Sprite(7);
 	static Sprite lorannDownRight = new Sprite(6);
 	static Sprite lorannDownLeft = new Sprite(8);
 	private static int key;
-	
-	static Sprite sprite [] = {lorannRight,lorannDownRight, lorannDown,lorannDownLeft,lorannLeft,lorannUpLeft,lorannUp,lorannUpRight};
-	
+	public boolean win = false;
+
+	static Sprite sprite[] = { lorannRight, lorannDownRight, lorannDown, lorannDownLeft, lorannLeft, lorannUpLeft,
+			lorannUp, lorannUpRight };
+
 	public Lorann() {
-		super(new Position(getPosition().getX(), getPosition().getY()),changeLorann(sens, 0));
+		super(new Position(getPosition().getX(), getPosition().getY()), changeLorann(sens, 0));
 	}
-	
+
+	public void setWin(boolean win) {
+		this.win = win;
+	}
+
+	public boolean getWin() {
+		return this.win;
+	}
+
 	public static Sprite changeLorann(int sens, int keyCode) {
 
-		if (key == KeyEvent.VK_UP || key == KeyEvent.VK_DOWN || key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_LEFT){
+		if (key == KeyEvent.VK_UP || key == KeyEvent.VK_DOWN || key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_LEFT) {
 			switch (sens) {
-				case 1:
-					return sprite[1];
-			
-				case 2:
-					return sprite[2];
-				
-				case 3:
-					return sprite[3];
-				
-				case 4:
-					return sprite[4];
-			
-				case 5:
-					return sprite[5];
-				
-				case 6:
-					return sprite[6];
-			
-				case 7:
-					return sprite[7];
-				
-				case 8:
-					return sprite[8];
-			
-				default:
+			case 1:
+				return sprite[1];
+
+			case 2:
+				return sprite[2];
+
+			case 3:
+				return sprite[3];
+
+			case 4:
+				return sprite[4];
+
+			case 5:
+				return sprite[5];
+
+			case 6:
+				return sprite[6];
+
+			case 7:
+				return sprite[7];
+
+			case 8:
+				return sprite[8];
+
+			default:
 				;
 			}
-		}
-		else {
-			for(int i = 0 ; i < sprite.length ; i++){
+		} else {
+			for (int i = 0; i < sprite.length; i++) {
 				return sprite[i];
 			}
 		}
 		return null;
-	}			
+	}
 }
