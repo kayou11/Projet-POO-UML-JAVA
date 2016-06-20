b07748a537d7f0e72874a82bca15106b795cef89
YannQuemar <yann.quemar1@gmail.com>
2016-06-20 09:26:06 +0200

    New Behavior AI Monster

--- model/src/main/java/elements/motion/Smart.java
+++ model/src/main/java/elements/motion/Smart.java
@@ -6,10 +6,22 @@ import contract.IMotionElement;
 
 public class Smart implements BehaviorAnimate {
 
-
 	public void animate(IMotionElement motionElement, ILorannWorld lorannWorld) {
-		// TODO Auto-generated method stub
-		
+		if (lorannWorld.getLorann().getY() < motionElement.getY()) {
+			motionElement.moveUp();
+		}
+		if (lorannWorld.getLorann().getY() > motionElement.getY()) {
+			motionElement.moveDown();
+		}
+		if (lorannWorld.getLorann().getY() == motionElement.getY()) {
+			if (lorannWorld.getLorann().getX() < motionElement.getX()) {
+				motionElement.moveLeft();
+			}
+			if (lorannWorld.getLorann().getY() > motionElement.getY()) {
+				motionElement.moveRight();
+			}
+		}
+
 	}
-	
+
 }
